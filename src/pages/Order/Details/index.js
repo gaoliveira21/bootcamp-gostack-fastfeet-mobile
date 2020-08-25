2a95d23f4eb5a404802aef2192b8fc370318764b
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-08-25 11:30:52 -0300

    Details page styles

--- src/pages/Order/Details/index.js
+++ src/pages/Order/Details/index.js
@@ -1,11 +1,75 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { Container, Header } from './styles';
+import {
+  Container,
+  Header,
+  DeliveryInfo,
+  DeliveryStatus,
+  TitleRow,
+  Title,
+  Label,
+  Value,
+  ContentRow,
+  Col,
+  Actions,
+  ActionBlock,
+  ActionText,
+} from './styles';
 
 const OrderDetails = () => {
   return (
     <Container>
       <Header />
+      <DeliveryInfo>
+        <TitleRow>
+          <Icon name="truck" size={24} color="#7D40E7" />
+          <Title>Informações da entrega</Title>
+        </TitleRow>
+        <Label>Destinatário</Label>
+        <Value>Vinicius Bueno</Value>
+
+        <Label>Endereço de entrega</Label>
+        <Value>Rua Beethoven, 1898, Diadema - SP, 0996-580</Value>
+
+        <Label>Produto</Label>
+        <Value>Yamaha SX7</Value>
+      </DeliveryInfo>
+
+      <DeliveryStatus>
+        <TitleRow>
+          <Icon name="calendar" size={24} color="#7D40E7" />
+          <Title>Informações da entrega</Title>
+        </TitleRow>
+        <Label>Status</Label>
+        <Value>Pendente</Value>
+
+        <ContentRow>
+          <Col>
+            <Label>Data de retirada</Label>
+            <Value>14/01/2020</Value>
+          </Col>
+          <Col>
+            <Label>Data de entrega</Label>
+            <Value>--/--/--</Value>
+          </Col>
+        </ContentRow>
+      </DeliveryStatus>
+
+      <Actions>
+        <ActionBlock>
+          <Icon name="close-circle-outline" size={28} color="#E74040" />
+          <ActionText>Informar {'\n'} Problema</ActionText>
+        </ActionBlock>
+        <ActionBlock>
+          <Icon name="information-outline" size={28} color="#E7BA40" />
+          <ActionText>Visualizar {'\n'} Problemas</ActionText>
+        </ActionBlock>
+        <ActionBlock>
+          <Icon name="check-circle-outline" size={28} color="#7D40E7" />
+          <ActionText>Confirmar {'\n'} Entrega</ActionText>
+        </ActionBlock>
+      </Actions>
     </Container>
   );
 };
--- src/pages/Order/Details/styles.js
+++ src/pages/Order/Details/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
@@ -8,4 +9,75 @@ export const Header = styled.View`
   width: 100%;
   height: 155px;
   background-color: #7d40e7;
+`;
+
+export const DeliveryInfo = styled.View`
+  border: 1px solid #f8f9fd;
+  background: #f8f9fd;
+  margin: -50px 20px 15px;
+  padding: 13px;
+  border-radius: 4px;
+`;
+
+export const DeliveryStatus = styled.View`
+  border: 1px solid #f8f9fd;
+  background: #f8f9fd;
+  margin: 0 20px 15px;
+  padding: 13px;
+  border-radius: 4px;
+`;
+
+export const TitleRow = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  margin-left: 5px;
+  color: #7d40e7;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export const Label = styled.Text`
+  color: #999999;
+  font-weight: bold;
+  font-size: 14px;
+  margin: 4px 0;
+  text-transform: uppercase;
+`;
+
+export const Value = styled.Text`
+  color: #555;
+  font-size: 14px;
+  margin-bottom: 15px;
+`;
+
+export const ContentRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Col = styled.View`
+  flex-direction: column;
+`;
+
+export const Actions = styled.View`
+  margin: 0 20px;
+  background: #f8f9fd;
+  border: 1px solid #f8f9fd;
+  border-radius: 4px;
+
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ActionBlock = styled(BorderlessButton)`
+  align-items: center;
+  padding: 20px;
+`;
+
+export const ActionText = styled.Text`
+  color: #999999;
+  text-align: center;
 `;
